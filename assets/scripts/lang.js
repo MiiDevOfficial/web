f921d172c9aea755ddddd587d2ad66e3a339dda5
jedrek0429 <a.bansleben0429@gmail.com>
2021-12-22 11:49:57 +0100

    button + img animations (ultra cute!!!)

--- assets/scripts/lang.js
+++ assets/scripts/lang.js
@@ -1,13 +1,23 @@
-const welcomeTexts = {
-    "pl": "Witamy na naszej stronie!",
-    "en": "Welcome to our page!",
-    "es": "¡Bienvenidos a nuestro sitio!"
+const langs = {
+    welcome: {
+        "pl": "Witamy na naszej stronie!",
+        "en": "Welcome to our page!",
+        "es": "¡Bienvenidos a nuestro sitio!"
+    },
+    start: {
+        "pl": "Rozpocznij",
+        "en": "Start",
+        "es": "Comienzo"
+    }
 }
 
 function lang(language) {
     const welcome = document.getElementById('welcome');
-    const welcomeText = document.querySelector("#welcome p")
+    const welcomeText = document.querySelector("#welcome p");
+    const startButton = document.querySelector("#welcome button")
     welcome.style.display = "flex";
-    welcomeText.innerHTML = welcomeTexts[language] || welcomeTexts["en"];
+    welcome.style.animation = "opac 2s"
+    welcomeText.innerHTML = langs.welcome[language] || langs.welcome["en"];
+    startButton.innerHTML = langs.start[language] || langs.start["en"];
     welcome.scrollIntoView();
 }
